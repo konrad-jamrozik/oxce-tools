6c9531d4529f0343185828662271f283a2ae136f
Konrad Jamrozik <kojamroz@microsoft.com>
2023-08-14 00:49:18 -0700

    fix namespace, add "oxce" to dict

--- cli-tests/UnitTest1.cs
+++ cli-tests/UnitTest1.cs
@@ -1,16 +1,15 @@
-namespace cli_tests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace OxceToolsTests;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
     }
 }
