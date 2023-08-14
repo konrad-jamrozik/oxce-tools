fc88d327fde7f71fd21c3ec2875c72c230aad355
Konrad Jamrozik <kojamroz@microsoft.com>
2023-08-14 00:39:44 -0700

    add cli-test from VS NUnit project template; update NuGet packages

---
+++ cli-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace cli_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
