a08d371bbd50e3d4bc4d8084266961a4e0906cbf
Konrad Jamrozik <kojamroz@microsoft.com>
2023-08-15 21:50:48 -0700

    properly handle "---" during yaml deserialization as treating the input as two docs

--- cli-tests/Tests.cs
+++ cli-tests/Tests.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -9,34 +11,6 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-    }
-
-    /// <summary>
-    /// Proof-of-concept showing round-tripping save file after making modification to it.
-    /// </summary>
-    [Test]
-    public void TestModifySaveFile2()
-    {
-        var stopwatch = Stopwatch.StartNew();
-        var saveDir = new SaveDir();
-        Console.Out.WriteLine($"Reading and deserializing save file from {saveDir.SaveFilePath}");
-        
-        string saveFileContents = File.ReadAllText(saveDir.SaveFilePath);
-        
-        // The save file has a header delimiter "---" that is not a valid yaml, hence we need to remove it (and later 
-        // on add it back.
-        // Actually:
-        // https://stackoverflow.com/questions/50788277/why-3-dashes-hyphen-in-yaml-file
-        // https://github.com/aaubry/YamlDotNet/wiki/Samples.DeserializingMultipleDocuments
-        saveFileContents = saveFileContents.Replace("---", "");
-        Console.Out.WriteLine("Time until read: " + stopwatch.Elapsed);
-
-        SaveFile saveFile = DeserializeSaveFile(saveFileContents);
-
-        Console.Out.WriteLine($"Time until yaml: {stopwatch.Elapsed}");
-
-        //List<object> alienMissions = (List<object>)yaml["alienMissions"];
-
     }
 
     /// <summary>
@@ -80,6 +54,72 @@ public class Tests
 
         Console.Out.WriteLine($"Time until done: {stopwatch.Elapsed}");
         // Assert: the modified save file can be loaded
+    }
+
+    /// <summary>
+    /// Proof-of-concept showing round-tripping save file after making modification to it.
+    /// </summary>
+    [Test]
+    public void TestModifySaveFile2()
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var saveDir = new SaveDir();
+        Console.Out.WriteLine($"Reading and deserializing save file from {saveDir.SaveFilePath}");
+        
+        string saveFileContents = File.ReadAllText(saveDir.SaveFilePath);
+        
+        saveFileContents = saveFileContents.Replace("---", "");
+        Console.Out.WriteLine("Time until read: " + stopwatch.Elapsed);
+
+        SaveFile saveFile = DeserializeSaveFile(saveFileContents);
+
+        Console.Out.WriteLine($"Time until yaml: {stopwatch.Elapsed}");
+
+        //List<object> alienMissions = (List<object>)yaml["alienMissions"];
+    }
+
+    // Properly handles --- 
+    // Based on:
+    // https://stackoverflow.com/questions/50788277/why-3-dashes-hyphen-in-yaml-file
+    // https://github.com/aaubry/YamlDotNet/wiki/Samples.DeserializingMultipleDocuments
+    [Test]
+    public void TestDeserializeSaveFileMetadata()
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var saveDir = new SaveDir();
+        Console.Out.WriteLine($"Reading and deserializing save file from {saveDir.SaveFilePath}");
+        
+        string saveFileContents = File.ReadAllText(saveDir.SaveFilePath);
+
+        var input = new StringReader(saveFileContents);
+
+        var deserializer = new DeserializerBuilder().Build();
+
+        var parser = new Parser(input);
+
+        // Consume the stream start event "manually"
+        parser.Consume<StreamStart>();
+
+        parser.Consume<DocumentStart>();
+        var saveMetadata = deserializer.Deserialize<Dictionary<object, object>>(parser);
+        Console.Out.WriteLine("## Save metadata");
+        foreach (var key in saveMetadata.Keys)
+        {
+            Console.Out.WriteLine(key);
+        }
+
+        parser.Consume<DocumentEnd>();
+        parser.Consume<DocumentStart>();
+        var saveData = deserializer.Deserialize<Dictionary<object, object>>(parser);
+        parser.Consume<DocumentEnd>();
+        Console.Out.WriteLine("## Save metadata");
+        foreach (var key in saveData.Keys)
+        {
+            Console.Out.WriteLine(key);
+        }
+
+        parser.Consume<StreamEnd>();
+        Assert.That(parser.Current, Is.Null);
     }
 
     private static Dictionary<object, object> Deserialize(string saveFileContents)
